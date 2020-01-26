601d8da4bba605544d8c5403229990365cead4e3
JoaoGeniselli <joao.g.geniselli@gmail.com>
2020-01-26 14:06:36 -0300

    Removing example tests.

--- domain/src/main/java/com/jgeniselli/banco/core/UnknownPlayerException.kt
+++
@@ -1,6 +0,0 @@
-package com.jgeniselli.banco.core
-
-class UnknownPlayerException(private val id: Long) : RuntimeException() {
-    override val message: String?
-        get() = "Player with id: $id is unknown"
-}
