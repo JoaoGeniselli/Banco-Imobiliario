84d843cf3ac4cc38e684b300b4e64a4af483dc46
João Geniselli <joao.g.geniselli@gmail.com>
2025-08-05 20:20:36 -0300

    replace all

--- app/src/main/java/com/jgeniselli/banco/game/transaction/history/HistoryRow.kt
+++
@@ -1,3 +0,0 @@
-package com.jgeniselli.banco.game.transaction.history
-
-data class HistoryRow(val formattedValue: String?, val iconColorHex: String, val valueColorHex: String)
--- domain/src/main/java/com/jgeniselli/banco/core/dto/StoredTransactionDto.kt
+++
@@ -1,3 +0,0 @@
-package com.jgeniselli.banco.core.dto
-
-data class StoredTransactionDto(val value: Double, val playerColor: String)
