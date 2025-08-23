7ae4fed1eb6d5158c30ccf627b8d780c5795defb
João Geniselli <joao.g.geniselli@gmail.com>
2025-08-23 12:26:38 -0300

    Versioning tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,24 @@ apply {
     from("$rootDir/quality/sonar/sonar-root.gradle")
     from("$rootDir/quality/jacoco/jacoco-merge.gradle")
 }
+
+tasks.register<Exec>("setupReleasePatch") {
+    group = "versioning"
+    description = "Increments the patch version for a new release."
+
+    commandLine("bash", "-c", "./scripts/setup_release.sh patch")
+}
+
+tasks.register<Exec>("setupReleaseMinor") {
+    group = "versioning"
+    description = "Increments the minor version and sets the patch version to 0 for a new release."
+
+    commandLine("bash", "-c", "./scripts/setup_release.sh minor")
+}
+
+tasks.register<Exec>("setupReleaseMajor") {
+    group = "versioning"
+    description = "Increments the major version and sets the minor and patch versions to 0 for a new release."
+
+    commandLine("bash", "-c", "./scripts/setup_release.sh major")
+}
